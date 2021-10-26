54f96b85f2853d21506d5f77a04d41fdbe403069
mirac <miracbalkaya029@gmail.com>
2021-10-26 18:21:34 +0300

    Initial commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Car {
+    public String NumberPlate;
+    public String CarColor;
+    public String CarType;
+
+    public String getNumberPlate() {
+        return NumberPlate;
+    }
+
+    public void setNumberPlate(String NumberPlate) {
+        this.NumberPlate = NumberPlate;
+    }
+
+    public String getCarColor() {
+        return CarColor;
+    }
+
+    public void setCarColor(String CarColor) {
+        this.CarColor = CarColor;
+    }
+
+    public String getCarType() {
+        return CarType;
+    }
+
+    public void setCarType(String CarType) {
+        this.CarType = CarType;
+    }
+}
---
+++ src/com/company/Payment.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Payment {
+
+    float HourAmount = 30;
+    float TotalAmountForHour = 0;
+    float TotalAmountForMinute = 0;
+
+    public float TotalAmount(int Hour, int Minute){
+        TotalAmountForHour = Hour * HourAmount;
+        if (Minute < 60 && Minute >= 30){
+            TotalAmountForMinute = 20;
+        }
+        else if (Minute < 30 && Minute >= 15){
+            TotalAmountForMinute = 15;
+        }
+        else if (Minute < 15 && Minute >= 1){
+            TotalAmountForMinute = 1;
+        }
+        return (TotalAmountForHour + TotalAmountForMinute);
+    }
+}
